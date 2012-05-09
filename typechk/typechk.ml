b5c6f72f7b4243b315f7cd3f71026e6d73eb2711
Helder Marques <hd.marques@campus.fct.unl.pt>
2012-05-09 23:20:08 +0100

    Updated 09/05/2012 23:03
    - Added comments with atention calls to wrong approaches
    - Changed name of typechck's EnvMap to TypeEnv
    - 'Compressed' functions with if's that returne true if true
    and returned false if false xP

--- typechk/typechk.ml
+++ typechk/typechk.ml
@@ -1,20 +1,16 @@
 open Blaise_syntax
 
-
 exception Invalid_type of string
 exception Id_not_found of string
 
-module EnvMap = Map.Make (String)
+module TypeEnvMap = Map.Make (String)
 
-
-type env = string*iType EnvMap.t
+type env = string*iType TypeEnvMap.t
 
 let find s env =
 	try
-		EnvMap.find s env
+		TypeEnvMap.find s env
 	with Not_found -> raise (Id_not_found s)
-
-
 
 let rec getType e =
 		match e with
@@ -47,41 +43,37 @@ let rec getType e =
 
 let bin_oper_int e1 e2 =
 	let (t1,t2) = (getType e1, getType e2) in
-		if t1 = TNumber && t2 = TNumber then true else false
+		t1 = TNumber && t2 = TNumber
 	
 
 let bin_oper_str e1 e2 =
 	let (t1,t2) = (getType e1, getType e2) in
-		if t1 = TString && t2 = TString then true else false
+		t1 = TString && t2 = TString
 
 
 let bin_oper_bool e1 e2 =
 	let (t1,t2) = (getType e1, getType e2) in
-		if t1 = TBoolean && t2 = TBoolean then true else false	
+		t1 = TBoolean && t2 = TBoolean
 	
 let un_oper_int e =
-	let t = getType e in
-	if t = TNumber then true else false
+	getType e = TNumber
 
 let un_oper_str e =
-	let t = getType e in
-	if t = TString then true else false
+	getType e = TString
 	
 let un_oper_bool e =
-	let t = getType e in
-	if t = TBoolean then true else false
+	getType e = TBoolean
 
 let rec un_oper_array t =
-	match t with
-		| TRef r -> (un_oper_array !r)
-		| TArray _ -> true
-		| _ -> false
+  	match t with
+  		| TRef r -> (un_oper_array !r)
+  		| TArray _ -> true
+  		| _ -> false
 	
 let type_of_array a = 
 	match a with
 		| TArray(_,t) -> t
-		| _ -> raise (Invalid_type "")
-
+		| _ -> raise (Invalid_type "Array expected")
 
 
 let rec typechkExp env e =
@@ -92,90 +84,126 @@ let rec typechkExp env e =
 		
 		| Boolean b -> Boolean(b)
 		
+		(* Este match esta mal porque o t que vem do parser e Undefined *)
+		(* logo deve-se usar wild card *)
+		(* e preciso usar um fold para verificar e correr o type check *)
+		(* para cada um *)
 		| Array(l,t) -> let size = List.length l in
-										List.iter(fun next -> 
-												let t' = getType next in
-												if t' <> t 
-												then raise (Invalid_type "array") ) l ; Array(l,TArray(size,t))
+  										List.iter(fun next -> 
+            												let t' = getType next in
+              												if t' <> t then 
+          															raise (Invalid_type "array") 
+          											) l ; 
+											Array(l,TArray(size,t))
 											
+		(* o mesmo que o anterior *)									
 		| Record(l,t) -> let l' = List.fold_left(fun prev (s,e) -> (s,t)::prev) [] l in
 												Record(l,TRecord(l'))
 											
 		| Add (e1, e2, _) -> let (e1',e2') = (typechkExp env e1,typechkExp env e2) in
-													if (bin_oper_int e1' e2') then Add(e1', e2', TNumber)
-													else if (bin_oper_str e1' e2') then Add(e1', e2', TString)
-													else raise (Invalid_type "")
+													if (bin_oper_int e1' e2') then 
+														Add(e1', e2', TNumber)
+													else if (bin_oper_str e1' e2') then 
+														Add(e1', e2', TString)
+													else 
+														raise (Invalid_type "")
 													
 			
 		| Sub (e1, e2, _) -> let (e1',e2') = (typechkExp env e1,typechkExp env e2) in
-													if (bin_oper_int e1' e2') then Sub(e1', e2', TNumber)
-													else raise (Invalid_type "")
+													if (bin_oper_int e1' e2') then 
+														Sub(e1', e2', TNumber)
+													else 
+														raise (Invalid_type "")
 													
 		| Compl (e1, _) -> let e1' = typechkExp env e1 in
-												if (un_oper_int e1') then Compl(e1', TNumber)
-												else raise (Invalid_type "")
+												if (un_oper_int e1') then 
+													Compl(e1', TNumber)
+												else 
+													raise (Invalid_type "")
 												
 		| Mult (e1, e2, _) -> let (e1',e2') = (typechkExp env e1,typechkExp env e2) in
-													if (bin_oper_int e1' e2') then Mult(e1', e2', TNumber)
-													else raise (Invalid_type "")
+													if (bin_oper_int e1' e2') then 
+														Mult(e1', e2', TNumber)
+													else 
+														raise (Invalid_type "")
 													
 		| Div (e1, e2, _) -> let (e1',e2') = (typechkExp env e1,typechkExp env e2) in
-													if (bin_oper_int e1' e2') then Div(e1', e2', TNumber)
-													else raise (Invalid_type "")
+													if (bin_oper_int e1' e2') then 
+														Div(e1', e2', TNumber)
+													else 
+														raise (Invalid_type "")
 													
 		| Mod (e1, e2, _) -> let (e1',e2') = (typechkExp env e1,typechkExp env e2) in
-													if (bin_oper_int e1' e2') then Mod(e1', e2', TNumber)
-													else raise (Invalid_type "")
+													if (bin_oper_int e1' e2') then 
+														Mod(e1', e2', TNumber)
+													else 
+														raise (Invalid_type "")
 													
 		| Eq (e1, e2, _) -> let (e1',e2') = (typechkExp env e1,typechkExp env e2) in
-													if (bin_oper_int e1' e2') || (bin_oper_str e1' e2')
-													then Eq(e1',e2',TBoolean)
-													else raise (Invalid_type "")
+													if (bin_oper_int e1' e2') || (bin_oper_str e1' e2') then 
+														Eq(e1',e2',TBoolean)
+													else 
+														raise (Invalid_type "")
 													
 		| Neq (e1, e2, _) -> let (e1',e2') = (typechkExp env e1,typechkExp env e2) in
-													if (bin_oper_int e1' e2') || (bin_oper_str e1' e2')
-													then Neq(e1',e2',TBoolean)
-													else raise (Invalid_type "")
+													if (bin_oper_int e1' e2') || (bin_oper_str e1' e2') then 
+														Neq(e1',e2',TBoolean)
+													else 
+														raise (Invalid_type "")
 													
 		| Gt (e1, e2, _) -> let (e1',e2') = (typechkExp env e1,typechkExp env e2) in
-													if (bin_oper_int e1' e2') || (bin_oper_str e1' e2')
-													then Gt(e1',e2',TBoolean)
-													else raise (Invalid_type "")
+													if (bin_oper_int e1' e2') || (bin_oper_str e1' e2') then 
+														Gt(e1',e2',TBoolean)
+													else 
+														raise (Invalid_type "")
 													
 		| Lt (e1, e2, _) -> let (e1',e2') = (typechkExp env e1,typechkExp env e2) in
-													if (bin_oper_int e1' e2') || (bin_oper_str e1' e2')
-													then Lt(e1',e2',TBoolean)
-													else raise (Invalid_type "")
+													if (bin_oper_int e1' e2') || (bin_oper_str e1' e2') then 
+														Lt(e1',e2',TBoolean)
+													else 
+														raise (Invalid_type "")
 													
 		| Gteq (e1, e2, _) -> let (e1',e2') = (typechkExp env e1,typechkExp env e2) in
 													if (bin_oper_int e1' e2') || (bin_oper_str e1' e2')
-													then Gteq(e1',e2',TBoolean)
-													else raise (Invalid_type "")
+													then 
+														Gteq(e1',e2',TBoolean)
+													else 
+														raise (Invalid_type "")
 													
 		| Lteq (e1, e2, _) -> let (e1',e2') = (typechkExp env e1,typechkExp env e2) in
-													if (bin_oper_int e1' e2') || (bin_oper_str e1' e2')
-													then Lteq(e1',e2',TBoolean)
-													else raise (Invalid_type "")
+													if (bin_oper_int e1' e2') || (bin_oper_str e1' e2') then 
+														Lteq(e1',e2',TBoolean)
+													else 
+														raise (Invalid_type "")
 													
 		| And (e1, e2, _) -> let (e1',e2') = (typechkExp env e1,typechkExp env e2) in
-													if (bin_oper_str e1' e2') then And(e1',e2',TBoolean)
-													else raise (Invalid_type "")
+													if (bin_oper_str e1' e2') then 
+														And(e1',e2',TBoolean)
+													else 
+														raise (Invalid_type "")
 													
 		| Or (e1, e2, _) -> let (e1',e2') = (typechkExp env e1,typechkExp env e2) in
-													if (bin_oper_int e1' e2') then Or(e1',e2',TBoolean)
-													else raise (Invalid_type "")
+													if (bin_oper_int e1' e2') then 
+														Or(e1',e2',TBoolean)
+													else 
+														raise (Invalid_type "")
 													
 		| Not (e1, _) -> let e1' = typechkExp env e1 in
-											if (un_oper_bool e1') then Not(e1',TBoolean)
-											else raise (Invalid_type "")
+											if (un_oper_bool e1') then 
+												Not(e1',TBoolean)
+											else 
+												raise (Invalid_type "")
 											
 		| Id (s, _) -> Id(s,getType(find s env))
 		
 		| GetArray(e1, e2, _) -> let(e1',e2') = (typechkExp env e1,typechkExp env e2) in 
 															let t1 = getType e1' in
-																if (un_oper_array t1) && (un_oper_int e2') 
-																then( let ty_array = type_of_array t1 in GetArray(e1',e2',ty_array))
-																else raise (Invalid_type "")
+																if (un_oper_array t1) && (un_oper_int e2') then( 
+																	let ty_array = type_of_array t1 in GetArray(e1',e2',ty_array)
+																)else 
+																	raise (Invalid_type "")
+																	
+		| _	-> Boolean(true)(* DUMB *)
 									
 (*		| GetRecord (e1, s, t) -> *)
 (*		| CallFun (e1,l,t) ->     *)
